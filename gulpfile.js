0896c78ed56c004af98877c231d76c645f4940b5
cefjoeii <cefjoeii@gmail.com>
2017-02-10 00:51:46 +0800

    Restructure directory and add new scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,23 @@
-// Not gulp tasks yet, mate.
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+
+gulp.task('sass', function() {
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS({keepSpecialComments : 0}))
+        .pipe(gulp.dest('src/css'));
+});
+
+gulp.task('sass:watch', function() {
+   gulp.watch('src/scss/**/*.scss', ['sass']); // ['sass'] watches the sass task
+});
+
+gulp.task('distCSS', function() { // deploy to dist
+    return gulp.src('src/css/main.css')
+        .pipe(rename('carverous.css'))
+        .pipe(gulp.dest('dist/css/'));
+});
