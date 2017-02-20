b72258fbcad6a9db372f297fbbc6c0a8d86c8721
cefjoeii <cefjoeii@gmail.com>
2017-02-20 22:53:54 +0800

    Refine alert and everything

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,28 @@
 "use strict";
 
+// Variables
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var cleanCSS = require('gulp-clean-css');
+var cleanCSS = require('gulp-clean-css');  // To minify and optionally remove comments
 var rename = require("gulp-rename");
+var flatten = require("gulp-flatten"); // To place all the output files in the same directory
+
+
+// Tasks
 
 gulp.task('sass', function() {
     return gulp.src('src/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        // .pipe(cleanCSS({keepSpecialComments : 0})) // minify and remove comments
-        .pipe(gulp.dest('src/css'));
+        .pipe(flatten())
+        // .pipe(cleanCSS({keepSpecialComments : 0}))
+        .pipe(gulp.dest('./src/css/'));
 });
-
 gulp.task('sass:watch', function() {
-   gulp.watch('src/scss/**/*.scss', ['sass']); // ['sass'] watches the 'sass' task
+   gulp.watch('src/scss/**/*.scss', ['sass']); // The ['sass'] re-runs the 'sass' task
 });
 
-gulp.task('deployCSS', function() { // deploy to dist
-    return gulp.src('src/css/main.css')
-        .pipe(rename('carverous.css'))
+gulp.task('deployCSS', function() { // Deploy to the dist directory
+    return gulp.src('src/css/carverous.css')
         .pipe(gulp.dest('dist/css/'));
 });
