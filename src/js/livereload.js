dd73b185c8e3b91255ce0c5f81ac0da82377a824
cefjoeii <cefjoeii@gmail.com>
2017-03-26 20:36:07 +0800

    Change blockquote style

---
+++ src/js/livereload.js
@@ -0,0 +1,8 @@
+// A livereload snippet to auto refresh a browser. Its software is required
+
+document.write(
+    '<script src="http://'
+    + (location.host || 'localhost').split(':')[0]
+    + ':35729/livereload.js?snipver=1"></'
+    + 'script>'
+)
