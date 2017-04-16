fc5e1c9a39373c58423ad4fa359e1b597c5b4350
cefjoeii <cefjoeii@gmail.com>
2017-04-16 11:06:34 +0800

    Make the dropdown toggle logic work

--- build/js/carverous.js
+++ build/js/carverous.js
@@ -12,6 +12,23 @@
 
 // Navbar
 
+var navDropdowns = document.querySelectorAll('.navbar-menu > .has-children > a');
+
+document.addEventListener('click', function (event) {
+  // console.log(event.target);
+
+  for (var i = 0; i < navDropdowns.length; i++) {
+
+    var navDropdown = navDropdowns[i];
+
+    if (event.target === navDropdown) {
+      navDropdown.parentElement.classList.toggle('active');
+    } else {
+      navDropdown.parentElement.classList.remove('active');
+    }
+  }
+});
+
 // Navicons
 // This is how simple it is to toggle in JS! (IE9+)
 // It is even possible to toggle using just the checkbox in CSS3! (Maybe next time?)
--- src/es/navbars.js
+++ src/es/navbars.js
@@ -1,1 +1,22 @@
 // Navbar
+
+let navDropdowns = document.querySelectorAll('.navbar-menu > .has-children > a');
+
+document.addEventListener('click', function(event) {
+  // console.log(event.target);
+
+  for (let i = 0; i < navDropdowns.length; i++) {
+
+    let navDropdown = navDropdowns[i];
+
+    if(event.target === navDropdown) {
+      navDropdown.parentElement.classList.toggle('active');
+    }
+    else {
+      navDropdown.parentElement.classList.remove('active');
+    }
+  }
+
+});
+
+
